054df481bdbb8b2f5ed17a3ad24d853c92dac4cc
jacob-machado-bwtek <118930827+jacob-machado-bwtek@users.noreply.github.com>
2024-05-07 15:27:46 -0400

    added basic running

--- LoxSharp/Program.cs
+++ LoxSharp/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!"); 
+﻿namespace CraftingInterpreters.CSLox;
+
+
+using System;
+using System.IO;
+using System.Text;
+
+public class Lox{
+    public static void Main(string[] args) {
+        if(args.Length > 1){
+            Console.WriteLine("Usage: CSLox[Script]");
+            Environment.Exit(64);
+        } else if (args.Length == 1){
+            RunFile(args[0]);
+        }else{
+            RunPrompt();
+        }
+    }
+
+ private static void RunPrompt()
+    {
+        Console.InputEncoding = Encoding.Default;
+        Console.OutputEncoding = Encoding.Default;
+
+        while (true)
+        {
+            Console.Write("> ");
+            string? line = Console.ReadLine();
+            if (line == null) break;
+            Run(line);
+        }
+    }
+    private static void RunFile(string path)
+    {
+        string content = File.ReadAllText(path, Encoding.Default);
+        Run(content);
+    }
+
+    private static void Run(string input)
+    {
+        Console.WriteLine($"Running: {input}");
+        
+    }
+}
