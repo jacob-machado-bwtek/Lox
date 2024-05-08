7743c7bb703295fbcac1e730577c9888d59e707c
jacob-machado-bwtek <118930827+jacob-machado-bwtek@users.noreply.github.com>
2024-05-08 09:41:24 -0400

    added two length tokens, peek

--- LoxSharp/Scanner.cs
+++ LoxSharp/Scanner.cs
@@ -1,6 +1,9 @@
 
 
 
+using System.Text.RegularExpressions;
+using CraftingInterpreters.LoxSharp;
+
 namespace LoxSharp
 {
     public interface IScanner {
@@ -64,7 +67,45 @@ namespace LoxSharp
                 case '+': AddToken(TokenType.PLUS);        break;
                 case ';': AddToken(TokenType.SEMICOLON);   break;
                 case '*': AddToken(TokenType.STAR);        break;
+
+                case '!': AddToken(Match('=') ? TokenType.BANG_EQUAL : TokenType.BANG);       break;
+                case '=': AddToken(Match('=') ? TokenType.EQUAL_EQUAL : TokenType.EQUAL);     break;
+                case '<': AddToken(Match('=') ? TokenType.LESS_EQUAL : TokenType.LESS);       break;
+                case '>': AddToken(Match('=') ? TokenType.GREATER_EQUAL : TokenType.GREATER); break;
+
+                case '/':
+                    if(Match('/')){
+                        while(peek() != '\n' && !IsAtEnd){
+                            advance();
+                        }
+                    }
+                    else{
+                        AddToken(TokenType.SLASH);
+                    }
+                    
+                break;
+
+
+                default: Lox.error(line, "Unexpected Character"); break;
+
             }
+        }
+
+        private char peek()
+        {
+            if(IsAtEnd) return '\0';
+            return source.ElementAt(current);
+
+        }
+
+        private bool Match(char expected)
+        {
+            if(IsAtEnd) return false;
+
+            if(source.ElementAt(current) != expected) return false;
+
+            current++;
+            return true;
         }
 
         private char advance(){
