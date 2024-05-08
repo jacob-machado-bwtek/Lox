e8f5febd5eedeea46dccb12b3c54c90fd9dcf269
jacob-machado-bwtek <118930827+jacob-machado-bwtek@users.noreply.github.com>
2024-05-08 09:27:27 -0400

    added ScanTokens

--- LoxSharp/Scanner.cs
+++ LoxSharp/Scanner.cs
@@ -1,3 +1,6 @@
+
+
+
 namespace LoxSharp
 {
     public interface IScanner {
@@ -20,6 +23,64 @@ namespace LoxSharp
         /// <returns>an empty list of Dummy Tokens</returns>
         public List<IToken> scanTokens(){
             return new List<IToken>();
+        }
+    }
+
+    public class Scanner : IScanner {
+        private readonly string source;
+        private readonly List<IToken> tokens = new List<IToken>();
+
+        private int start = 0;
+        private int current = 0;
+        private int line = 1;
+
+        private bool IsAtEnd {get{return current >= source.Length;}}
+        
+        public Scanner(string input){
+            source = input;
+        } 
+
+        public List<IToken> scanTokens(){
+            while(!IsAtEnd){
+                start = current;
+                scanToken(); 
+            }
+            tokens.Add(new Token(TokenType.EOF,"",null,line));
+            return tokens;
+        }
+
+        private void scanToken()
+        {
+            char c = advance();
+
+            switch(c){
+                case '(': AddToken(TokenType.LEFT_PAREN);  break;
+                case ')': AddToken(TokenType.RIGHT_PAREN); break;
+                case '{': AddToken(TokenType.LEFT_BRACE);  break;
+                case '}': AddToken(TokenType.RIGHT_BRACE); break;
+                case ',': AddToken(TokenType.COMMA);       break;
+                case '.': AddToken(TokenType.DOT);         break;
+                case '-': AddToken(TokenType.MINUS);       break;
+                case '+': AddToken(TokenType.PLUS);        break;
+                case ';': AddToken(TokenType.SEMICOLON);   break;
+                case '*': AddToken(TokenType.STAR);        break;
+            }
+        }
+
+        private char advance(){
+            var c = source.ElementAt(current);
+            current++;
+            return c;
+        }
+
+        private void AddToken(TokenType type){
+            AddToken(type,null);
+        }
+
+        private void AddToken(TokenType type, object? value)
+        {
+            String text = source.Substring(start,current);
+            tokens.Add(new Token(type,text,value,line));
         }
     }
     
--- LoxSharp/Token.cs
+++ LoxSharp/Token.cs
@@ -1,9 +1,47 @@
-namespace LoxSharp
-{
-    public interface IToken {
-       
-    }
+namespace LoxSharp;
+
+
+public interface IToken {
+
+    public TokenType type { get;}
+
+    public string lexeme {get;}
+
+    public Object? literal {get;}
+
+    public int line {get;}
+    string ToString();
+
+
+}
     
 
-    public class DummyToken : IToken {}
+public class Token : IToken {
+    public TokenType type {get;}
+    public  string lexeme {get;}
+    public object? literal
+    {
+        get;
+        private set;
+    }
+    public  int line {get;}
+
+    public Token(TokenType type, string lexeme, Object? literal, int line) {
+        this.type = type;
+        this.lexeme = lexeme;
+        this.literal = literal;
+        this.line = line;
+    }
+
+
+    public override string ToString(){
+        var s = $"{type} {lexeme} {literal}";
+        return s;
+    }
+
 }
+
+
+
+
+    
