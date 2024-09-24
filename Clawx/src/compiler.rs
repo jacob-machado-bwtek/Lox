b0c4d170daffc2b9eeb32dde4d48d98af817f47f
jacob-machado-bwtek <118930827+jacob-machado-bwtek@users.noreply.github.com>
2024-09-24 14:22:21 -0400

    16.2 A Token at a Time

--- Clawx/src/compiler.rs
+++ Clawx/src/compiler.rs
@@ -1,6 +1,9 @@
 use thiserror::Error;
 
-use crate::scanner::Scanner;
+use crate::{
+    scanner::{Scanner, TokenKind},
+    types::Line,
+};
 
 #[derive(Error,Debug)]
 pub enum Error{}
@@ -9,6 +12,24 @@ pub type Result<T = (), E = Error> = std::result::Result<T,E>;
 
 
 pub fn compile(source: &str) -> Result {
-    let scanner = Scanner::new(source);
+    let mut scanner = Scanner::new(source);
+
+    let mut line = Line(0);
+
+    loop {
+        let token = scanner.scan();
+
+        if token.line != line {
+            print!("{:>4}", *token.line);
+            line = token.line;
+        } else {
+            print!("  | ");
+        }
+        println!("{:2} '{}'", u8::from(token.kind.clone()), token.lexeme);
+
+        if token.kind == TokenKind::Eof {
+            break;
+        };
+    }
     Ok(())
 }
